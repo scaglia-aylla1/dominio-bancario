aa18dcf4568c8e572546b0dfa4d195ef42a1bd36
Aylla Scaglia <aylla@scaglia.com.br>
2025-02-17 19:35:00 -0300

    Initial commit

---
+++ src/test/java/com/studo/dominio/bancario/DominioBancarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studo.dominio.bancario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioBancarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
